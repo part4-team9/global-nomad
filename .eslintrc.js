35b375fbd940208f670e0d1247d4680b54c4f14b
eundonghyeok <mephisto7191@naver.com>
2024-07-25 21:25:00 +0900

    Init: 프로젝트 초기 세팅 옵션 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
   extends: ['./eslint/react.js', './eslint/typescript.js', './eslint/import.js', './eslint/prettier.js', './eslint/next.js', './eslint/custom-rules.js'],
   plugins: [],
