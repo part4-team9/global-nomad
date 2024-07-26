b0f63e1f1c6c6f2c5ffd6257f685238bc73c65cc
hongseoha <seoseo.ha.h@gmail.com>
2024-07-27 02:18:11 +0900

    Feat: 로고 클릭시 메인페이지 이동

--- app/_components/header/index.tsx
+++ app/_components/header/index.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="fixed left-0 right-0 top-0 z-30 w-full bg-white shadow-sm">
       <section className="mx-auto flex h-[70px] content-center justify-between px-6 py-[21px] lg:max-w-[1200px]">
-        <Link href="#" className="relative h-[28px] w-auto">
+        <Link href="/main" className="relative h-[28px] w-auto">
           <Image src={logoWithTitle} alt="Logo" />
         </Link>
         <nav className="">
