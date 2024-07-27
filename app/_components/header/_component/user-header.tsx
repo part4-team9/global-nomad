73cd62a9e40aa0861ee495e55aff11aa76298841
SeohaH <122506809+hongseoha@users.noreply.github.com>
2024-07-27 16:34:27 +0900

    Feat: 헤더 컴포넌트 제작 (#10)

---
+++ app/_components/header/_component/user-header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import gnbProfileImg from 'public/assets/images/gnb-profile.png';
+
+import alarm from 'public/assets/icons/alarm.svg';
+
+export default function UserHeader() {
+  return (
+    <div className="flex text-m">
+      <div className="my-[7px] border-r-[1px] border-gray-200 pr-3 md:pr-[25px]">
+        <Image src={alarm} alt="alarm" />
+      </div>
+      <div className="ml-3 flex justify-between gap-[10px] md:ml-[25px]">
+        <div className="h-8 w-8 overflow-hidden rounded-full">
+          <Image src={gnbProfileImg} alt="profile image" />
+        </div>
+        <div className="py-[5px]">
+          정만철
+          {/* 추후 userName으로 변경 */}
+        </div>
+      </div>
+    </div>
+  );
+}
