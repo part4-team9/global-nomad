a27526b4a3ef9f5445e874de16e2c0c559961a50
eunbinnie <dmsqlsdl54@gmail.com>
2024-07-30 01:04:12 +0900

    Feat: common layout 컴포넌트 작업

--- app/_components/common-layout/index.tsx
+++ app/_components/common-layout/index.tsx
@@ -1,9 +1,13 @@
 import type { PropsWithChildren } from 'react';
 
-function CommonLayout({ children }: PropsWithChildren) {
+interface CommonLayoutProps extends PropsWithChildren {
+  className?: string;
+}
+
+function CommonLayout({ children, className }: CommonLayoutProps) {
   return (
     <div className="mx-auto w-full max-w-[1248px] px-4 mobile:px-6">
-      <div className="pb-[136px] pt-6 mobile:pb-[312px] tablet:pb-[344px] tablet:pt-[72px]">{children}</div>
+      <div className={`commonLayoutWrap ${className}`}>{children}</div>
     </div>
   );
 }
