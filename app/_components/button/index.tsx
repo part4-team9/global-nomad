be051d19a711bc7ff1eaaf17e07de57021a68240
유성진 <141720486+yubugi@users.noreply.github.com>
2024-07-30 18:49:21 +0900

    fix: 추가 수정

--- app/_components/button/index.tsx
+++ app/_components/button/index.tsx
@@ -1,5 +1,17 @@
 'use client';
 
+/**
+ * @interface ButtonProps
+ * @property borderRadius - borderRadius 기본값 '4px'
+ * @property className - 추가적인 CSS 설정
+ * @property disabled - 버튼 비활성화
+ * @property fontSize - 텍스트의 폰트 크기를 설정
+ * @property isSelected - 버튼이 선택 여부 결정 ture이면 색상변화
+ * @property onClick - 버튼 클릭 이벤트 핸들러
+ * @property type - 버튼의 동작
+ * @property variant - 버튼의 색상
+ */
+
 interface ButtonProps {
   borderRadius?: string;
   children: React.ReactNode;
@@ -8,7 +20,8 @@ interface ButtonProps {
   fontSize?: 'm' | 'base';
   isSelected?: boolean;
   onClick?: () => void;
-  variant: 'select' | 'action';
+  type?: 'button' | 'submit' | 'reset';
+  variant: 'white' | 'black';
 }
 
 const fontSizeClass = {
@@ -23,7 +36,8 @@ export default function Button({
   className = '',
   fontSize = 'base',
   borderRadius = '4px',
-  variant,
+  variant = 'white',
+  type = 'button',
   isSelected = false,
 }: ButtonProps) {
   const baseStyle = `
@@ -32,9 +46,9 @@ export default function Button({
   `;
 
   let stateStyles = '';
-  if (variant === 'select') {
+  if (variant === 'white') {
     stateStyles = isSelected ? `bg-nomad-black text-white` : `bg-white text-nomad-black hover:bg-gray-100`;
-  } else if (variant === 'action') {
+  } else if (variant === 'black') {
     stateStyles = disabled ? `bg-gray-500 text-white` : `bg-nomad-black text-white hover:bg-white hover:text-nomad-black`;
   }
 
@@ -42,7 +56,8 @@ export default function Button({
   const buttonStyle = `${baseStyle} ${stateStyles} ${className} ${borderStyle}`;
 
   return (
-    <button className={buttonStyle} onClick={onClick} disabled={disabled} type="button" style={{ borderRadius }}>
+    // eslint-disable-next-line react/button-has-type
+    <button className={buttonStyle} onClick={onClick} disabled={disabled} type={type} style={{ borderRadius }}>
       {children}
     </button>
   );
