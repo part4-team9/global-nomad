82bdbc628d273b6b7c11f0e8616c7e3c3065eae6
hongseoha <seoseo.ha.h@gmail.com>
2024-08-02 03:16:44 +0900

    Refactor: input render 함수 분리, input props 객체 분리

--- app/(route)/(auth)/login/_components/log-in-form.tsx
+++ app/(route)/(auth)/login/_components/log-in-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import type { FieldError, RegisterOptions } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { postLogin } from '@/_apis/authentication';
@@ -14,6 +15,43 @@ type FormValues = {
   email: string;
   password: string;
 };
+
+interface InputField {
+  id: keyof FormValues;
+  label: string;
+  placeholder: string;
+  type: string;
+  validation: RegisterOptions<FormValues>;
+}
+
+const inputFields: InputField[] = [
+  {
+    id: 'email',
+    label: '이메일',
+    type: 'email',
+    placeholder: '이메일을 입력해 주세요',
+    validation: {
+      required: '이메일은 필수 항목입니다.',
+      pattern: {
+        value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
+        message: '잘못된 이메일 입니다.',
+      },
+    },
+  },
+  {
+    id: 'password',
+    label: '비밀번호',
+    type: 'password',
+    placeholder: '비밀번호를 입력해 주세요',
+    validation: {
+      required: '비밀번호는 필수 항목입니다.',
+      minLength: {
+        value: 8,
+        message: '8자 이상 입력해주세요.',
+      },
+    },
+  },
+];
 
 function LoginForm() {
   const router = useRouter();
@@ -35,7 +73,6 @@ function LoginForm() {
         router.replace('/main');
       }
     } catch (error) {
-
       let message = '로그인 중 오류가 발생했습니다.';
       if (error instanceof Error) {
         message = error.message;
@@ -44,56 +81,36 @@ function LoginForm() {
       openModal();
     }
   });
+
+  const renderInput = (id: keyof FormValues, label: string, type: string, placeholder: string, validation: RegisterOptions<FormValues>, error?: FieldError) => (
+    <div className="grid gap-2" key={id}>
+      <label htmlFor={id}>{label}</label>
+      <Input id={id} type={type} placeholder={placeholder} error={Boolean(error)} errorMessage={error?.message} {...register(id, validation)} />
+    </div>
+  );
+
   return (
     <>
       <form noValidate onSubmit={handleForm} className="grid w-full gap-7 px-3 md:px-0">
-        <div className="grid w-full gap-2">
-          <label htmlFor="email">이메일</label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="이메일을 입력해 주세요"
-            error={Boolean(errors?.email)}
-            errorMessage={errors?.email?.message}
-            {...register('email', {
-              required: '이메일은 필수 항목입니다.',
-              pattern: {
-                value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-                message: '잘못된 이메일 입니다.',
-              },
-            })}
-          />
-        </div>
-        <div className="grid gap-2">
-          <label htmlFor="password">비밀번호</label>
-          <Input
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력해 주세요"
-            error={Boolean(errors?.password)}
-            errorMessage={errors?.password?.message}
-            {...register('password', {
-              required: '비밀번호는 필수 항목입니다.',
-              minLength: {
-                value: 8,
-                message: '8자 이상 입력해주세요.',
-              },
-            })}
-          />
-        </div>
+        {inputFields.map((field) => renderInput(field.id, field.label, field.type, field.placeholder, field.validation, errors[field.id]))}
+        
+        {/* 버튼 컴포넌트로 변경 */}
         <button type="submit" disabled={isSubmitting} className="border bg-gray-100 py-[14px]">
           로그인
         </button>
-        {/* 버튼 컴포넌트로 변경 */}
+        
       </form>
+
       <Modal isOpen={isOpen} onClose={closeModal}>
-        <div className="m-auto px-[90px] pb-[28px] pt-[26px] text-right text-[18px] md:w-[540px] md:px-[33px]">
+        <div className="m-auto px-[90px] pb-[28px] pt-[26px] text-right text-lg md:w-[540px] md:px-[33px]">
           <p className="pb-[43px] pt-[53px] text-center">{modalMessage}</p>
           <span className="flex justify-center md:justify-end">
-            <button type="button" onClick={closeModal} className="h-[42px] w-[138px] rounded-[8px] bg-black text-white">
+            
+            {/* 버튼 컴포넌트로 변경 */}
+            <button type="button" onClick={closeModal} className="h-[42px] w-[138px] rounded-lg bg-black text-white">
               확인
             </button>
-            {/* 버튼 컴포넌트로 변경 */}
+
           </span>
         </div>
       </Modal>
--- app/(route)/(auth)/signup/_components/sign-up-form.tsx
+++ app/(route)/(auth)/signup/_components/sign-up-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import type { FieldError, RegisterOptions } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { postSignup } from '@/_apis/user';
@@ -17,6 +18,14 @@ type FormValues = {
   password: string;
 };
 
+interface InputField {
+  id: keyof FormValues;
+  label: string;
+  placeholder: string;
+  type: string;
+  validation: RegisterOptions<FormValues>;
+}
+
 function SignUpForm() {
   const router = useRouter();
   const {
@@ -32,6 +41,58 @@ function SignUpForm() {
 
   const password = watch('password', '');
 
+  const inputFields: InputField[] = [
+    {
+      id: 'email',
+      label: '이메일',
+      type: 'email',
+      placeholder: '이메일을 입력해 주세요',
+      validation: {
+        required: '이메일은 필수 항목입니다.',
+        pattern: {
+          value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
+          message: '잘못된 이메일 입니다.',
+        },
+      },
+    },
+    {
+      id: 'nickname',
+      label: '닉네임',
+      type: 'nickname',
+      placeholder: '닉네임을 입력해주세요.',
+      validation: {
+        required: '닉네임은 필수 항목입니다.',
+        maxLength: {
+          value: 10,
+          message: '닉네임은 10글자 이내로 작성해주세요',
+        },
+      },
+    },
+    {
+      id: 'password',
+      label: '비밀번호',
+      type: 'password',
+      placeholder: '비밀번호를 입력해 주세요',
+      validation: {
+        required: '비밀번호는 필수 항목입니다.',
+        minLength: {
+          value: 8,
+          message: '8자 이상 입력해주세요.',
+        },
+      },
+    },
+    {
+      id: 'confirmPassword',
+      label: '비밀번호 확인',
+      type: 'password',
+      placeholder: '비밀번호를 한 번 더 입력해 주세요',
+      validation: {
+        required: '비밀번호 확인은 필수 항목입니다.',
+        validate: (value) => value === password || '비밀번호가 일치하지 않습니다.',
+      },
+    },
+  ];
+
   const handleForm = handleSubmit(async (data: FormValues) => {
     try {
       const result = await postSignup(data);
@@ -41,7 +102,6 @@ function SignUpForm() {
         openModal();
       }
     } catch (error) {
-
       let message = '회원가입 중 오류가 발생했습니다.';
       if (error instanceof Error) {
         message = error.message;
@@ -58,90 +118,34 @@ function SignUpForm() {
     closeModal();
   };
 
+  const renderInput = (id: keyof FormValues, label: string, type: string, placeholder: string, validation: RegisterOptions<FormValues>, error?: FieldError) => (
+    <div className="grid gap-2" key={id}>
+      <label htmlFor={id}>{label}</label>
+      <Input id={id} type={type} placeholder={placeholder} error={Boolean(error)} errorMessage={error?.message} {...register(id, validation)} />
+    </div>
+  );
+
   return (
     <>
       <form noValidate onSubmit={handleForm} className="grid w-full gap-7 px-3 md:px-0">
-        <div className="grid w-full gap-2">
-          <label htmlFor="email">이메일</label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="이메일을 입력해 주세요"
-            error={Boolean(errors?.email)}
-            errorMessage={errors?.email?.message}
-            {...register('email', {
-              required: '이메일은 필수 항목입니다.',
-              pattern: {
-                value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-                message: '잘못된 이메일 입니다.',
-              },
-            })}
-          />
-        </div>
-        <div className="grid gap-2">
-          <label htmlFor="nickname">닉네임</label>
-          <Input
-            id="nickname"
-            type="text"
-            placeholder="닉네임을 입력해 주세요"
-            error={Boolean(errors?.nickname)}
-            errorMessage={errors?.nickname?.message}
-            {...register('nickname', {
-              required: '닉네임은 필수 항목입니다.',
-              maxLength: {
-                value: 10,
-                message: '닉네임은 10글자 이내로 작성해주세요',
-              },
-            })}
-          />
-        </div>
-
-        <div className="grid gap-2">
-          <label htmlFor="password">비밀번호</label>
-          <Input
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력해 주세요"
-            error={Boolean(errors?.password)}
-            errorMessage={errors?.password?.message}
-            {...register('password', {
-              required: '비밀번호는 필수 항목입니다.',
-              minLength: {
-                value: 8,
-                message: '8자 이상 입력해주세요.',
-              },
-            })}
-          />
-        </div>
-
-        <div className="grid gap-2">
-          <label htmlFor="confirmPassword">비밀번호 확인</label>
-          <Input
-            id="confirmPassword"
-            type="password"
-            placeholder="비밀번호를 한 번 더 입력해 주세요"
-            error={Boolean(errors?.confirmPassword)}
-            errorMessage={errors?.confirmPassword?.message}
-            {...register('confirmPassword', {
-              required: '비밀번호 확인은 필수 항목입니다.',
-              validate: (value) => value === password || '비밀번호가 일치하지 않습니다.',
-            })}
-          />
-        </div>
-
+        {inputFields.map((field) => renderInput(field.id, field.label, field.type, field.placeholder, field.validation, errors[field.id]))}
+        
+        {/* 버튼 컴포넌트로 변경 */}
         <button type="submit" disabled={isSubmitting} className="border bg-gray-100 py-[14px]">
           회원가입
         </button>
-        {/* 버튼 컴포넌트로 변경 */}
+
       </form>
       <Modal isOpen={isOpen} onClose={handleModalClose}>
-        <div className="m-auto px-[90px] pb-[28px] pt-[26px] text-right text-[18px] md:w-[540px] md:px-[33px]">
+        <div className="m-auto px-[90px] pb-[28px] pt-[26px] text-right text-lg md:w-[540px] md:px-[33px]">
           <p className="pb-[43px] pt-[53px] text-center">{modalMessage}</p>
           <span className="flex justify-center md:justify-end">
+            
+            {/* 버튼 컴포넌트로 변경 */}
             <button type="button" onClick={handleModalClose} className="h-[42px] w-[138px] rounded-[8px] bg-black text-white">
               확인
             </button>
-            {/* 버튼 컴포넌트로 변경 */}
+
           </span>
         </div>
       </Modal>
