f36e49e04120b8d266174ee076e87b6a2239044a
yubugi <ysj7645@gmail.com>
2024-08-12 03:47:11 +0000

    Fix: tailwindCSS confing type 불일치 문제 해결 (#46)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss';
-
 import animation from './styles/tailwind.animation';
 import colors from './styles/tailwind.colors';
 import keyframes from './styles/tailwind.keyframes';
@@ -7,7 +5,8 @@ import screens from './styles/tailwind.screens';
 import boxShadow from './styles/tailwind.shadow';
 import typography from './styles/tailwind.typography';
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
@@ -20,6 +19,6 @@ const config: Config = {
     },
   },
   plugins: [],
-} satisfies Config;
+};
 
 export default config;
