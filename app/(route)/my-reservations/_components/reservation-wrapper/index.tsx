9abbb6c06ef593b73388bbbca13682d2fd6cb775
eunbinnie <dmsqlsdl54@gmail.com>
2024-08-12 12:29:45 +0900

    Chore: 안쓰는 파일 삭제

--- app/(route)/my-reservations/_components/reservation-wrapper/index.tsx
+++ app/(route)/my-reservations/_components/reservation-wrapper/index.tsx
@@ -1,9 +1,5 @@
 import type { Reservations } from '@/_types/myReservations';
 
-import DateAndGuests from '@/_components/date-and-guests';
-
-import StatusBoard from '../status-board';
-
 interface ReservationsWrapper {
   datas: Reservations[];
 }
@@ -12,10 +8,7 @@ function ReservationWrapper({ datas }: ReservationsWrapper) {
   return (
     <div className="mt-3 grid gap-2 mobile:mt-6 mobile:gap-4 tablet:mt-4 tablet:gap-6">
       {datas.map((data) => (
-        <div key={data.activity.id}>
-          <StatusBoard status={data.status} />
-          <DateAndGuests date={data.date} startTime={data.startTime} endTime={data.endTime} headCount={data.headCount} />
-        </div>
+        <div key={data.activity.id} />
       ))}
     </div>
   );
