1bee15f417432092fe874db7176f553f48efb001
Hyegyeong Kim <168407977+i-nooo-2@users.noreply.github.com>
2024-08-14 16:13:23 +0900

    Feat: Side User Profile Card Component 구현 (#42)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import NoticeCalender from './_components/calendar-notice';
+import StickyLayout from './_components/side-sticky-layout';
+import SideUserProfileCard from './_components/side-user-profile-card';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <NoticeCalender /> */}
-      <a href='/my'>마이페이지 이동</a>
+    <main>
+      <StickyLayout />
+      <a href="/my">마이페이지 이동</a>
     </main>
   );
 }
