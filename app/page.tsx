99efd041085617c49baa720377804f0afcbd8885
eunbinnie <dmsqlsdl54@gmail.com>
2024-08-19 21:55:08 +0900

    Feat: 메인 배너 퍼블리싱

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
-import StickyLayout from './_components/side-sticky-layout';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Button from './_components/button';
+
+import Logo from 'public/assets/icons/banner-logo.svg';
 
 export default function Home() {
   return (
-    <main>
-      <StickyLayout />
-      <a href="/my">마이페이지 이동</a>
-    </main>
+    <section
+      className="relative bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('/assets/images/main-banner.png')] bg-cover bg-no-repeat pb-20 pt-[120px] tablet:pb-[90px] tablet:pt-[200px]"
+      style={{ backgroundPosition: 'center' }}
+    >
+      <div className="z-[1] mx-auto grid max-w-[1248px] gap-10 px-6 tablet:gap-20">
+        <div className="mobile:max-w-[80%]">
+          <Image src={Logo} alt="GlobalNomad" />
+        </div>
+        <p className="grid gap-5 break-keep text-white">
+          <span className="text-2xl font-medium leading-[1.2] tablet:text-[40px]">당신의 여정이 매 순간 즐거울 수 있도록</span>
+          <span className="text-lg font-medium leading-normal tablet:text-[27px]">여행에 활력을 더하다, 글로벌 노마드</span>
+        </p>
+        <div className="flex flex-wrap gap-5 tablet:mt-[100px]">
+          <Link href="/main">
+            <Button variant="white" className="h-12 w-36 border-none bg-[rgba(255,255,255,0.8)]">
+              둘러보기
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button variant="white" className="h-12 w-36 border-none bg-[rgba(255,255,255,0.8)]">
+              가입하러 가기
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
