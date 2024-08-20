d9f19b3e3b0b4636e0696fe517300bd43300df32
eundonghyeok <mephisto7191@naver.com>
2024-08-20 17:45:42 +0900

    Refactor: 레이아웃 시프트 방지

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,12 +7,12 @@ import Button from './_components/button';
 export default function NotFound() {
   return (
     <div className="flex min-h-dvh flex-col items-center justify-center">
-      <Image src={Error} alt="404" priority className="z-0 tablet:max-w-[700px] mobile:max-w-[500px]" />
+      <Image src={Error} alt="404" width={700} height={700} priority className="z-0 mb-8 mobile:max-w-[500px] tablet:max-w-[700px]" />
       <div className="flex flex-col items-center gap-3 text-center">
-        <h2 className="mx-12 text-4xl mobile:text-7xl">Not Found</h2>
+        <h1 className="mx-12 text-4xl mobile:text-7xl">Not Found</h1>
         <p className="text-lg font-bold mobile:text-2xl">페이지를 찾을 수 없습니다!</p>
         <Link href="/" className="flex h-[40px] w-full items-center justify-center mobile:h-[50px]">
-          <Button variant="black" className="size-full text-2lg tablet:text-2xl mobile:text-xl">
+          <Button variant="black" className="size-full text-2lg mobile:text-xl tablet:text-2xl">
             Home
           </Button>
         </Link>
