9dcba754329b51853da1288bfbb54e6d349dac29
eundonghyeok <mephisto7191@naver.com>
2024-08-25 02:41:21 +0900

    Fix: 빌드 오류 해결

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import KakaoProvider from 'next-auth/providers/kakao';
 
-export const authOption: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -17,6 +17,4 @@ export const authOption: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOption);
-
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
