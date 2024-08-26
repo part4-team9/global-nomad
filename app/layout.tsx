048448e0762c14cd77f06611e5077e10778fe56b
yubugi <ysj7645@gmail.com>
2024-08-26 10:09:59 +0000

    Feat: 랜딩 페이지 제작 (#65)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 
+import type { Metadata } from 'next';
+
 import Footer from './_components/Footer';
 import Header from './_components/header';
 import Providers from './providers';
+
+export const metadata: Metadata = {
+  title: 'GlobalNomad',
+  icons: {
+    icon: '/assets/icons/logo.svg',
+  },
+};
 
 export default function RootLayout({
   children,
