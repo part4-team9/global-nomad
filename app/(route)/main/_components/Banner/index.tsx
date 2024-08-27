0122d57382891b13e3a1a54ba461194526bd6936
eundonghyeok <mephisto7191@naver.com>
2024-08-27 16:47:51 +0900

    Feat: Banner Carousel 제작

--- app/(route)/main/_components/Banner/index.tsx
+++ app/(route)/main/_components/Banner/index.tsx
@@ -1,85 +1,13 @@
-'use client';
-
-import { useState } from 'react';
-import Image from 'next/image';
-
-import useGetActivities from '@/_hooks/activities/useGetActivities';
-
-import Btn from 'public/assets/icons/carousel-btn.svg';
-import Spinner from 'public/assets/icons/spinner.svg';
-
-const calendarNum = new Date().getMonth() + 1;
+import Carousel from '@/_components/Carousel';
 
 /**
  * Banner 컴포넌트 입니다.
  */
 
 export default function Banner() {
-  const [currentImg, setCurrentImg] = useState(0);
-  const [transition, setTransition] = useState('opacity-100');
-
-  const { data, isLoading, isError } = useGetActivities({
-    method: 'cursor',
-    cursorId: null,
-    size: 3,
-    sort: 'most_reviewed',
-  });
-
-  const handlePrevClick = () => {
-    if (data) {
-      setTransition('opacity-85');
-      setTimeout(() => {
-        setCurrentImg((prev) => (prev + 1) % data.activities.length);
-        setTransition('opacity-100');
-      }, 300);
-    }
-  };
-
-  const handleNextClick = () => {
-    if (data) {
-      setTransition('opacity-85');
-      setTimeout(() => {
-        setCurrentImg((prevIndex) => (prevIndex - 1 + data.activities.length) % data.activities.length);
-        setTransition('opacity-100');
-      }, 300);
-    }
-  };
-
   return (
     <div className="relative h-[240px] w-full mobile:h-[540px]">
-      {isLoading ? (
-        <Image src={Spinner} fill alt="loading" />
-      ) : (
-        data && (
-          <div className="absolute inset-0">
-            <Image
-              src={data?.activities[currentImg].bannerImageUrl}
-              alt={data?.activities[0].title}
-              fill
-              className={`object-cover transition-opacity duration-300 ${transition}`}
-            />
-            <div className="absolute inset-0 z-30 flex items-center justify-between mobile:px-2 tablet:px-4">
-              <button type="button" onClick={handlePrevClick}>
-                <Image src={Btn} alt="이전 버튼" width={40} height={40} className="size-8 mobile:size-16" />
-              </button>
-              <button type="button" onClick={handleNextClick}>
-                <Image src={Btn} alt="이전 버튼" width={40} height={40} className="size-8 rotate-180 mobile:size-16" />
-              </button>
-            </div>
-          </div>
-        )
-      )}
-      <div className="relative z-10 size-full bg-bannerGradient" />
-      <div className="relative bottom-[170px] z-20 mx-auto max-w-[1200px] px-8 font-bold text-white mobile:bottom-[360px] mobile:px-20">
-        <h4 className="w-[184px] break-keep text-xl mobile:w-[440px] mobile:text-[40px] mobile:leading-[60px] tablet:w-[502px] tablet:text-5xl tablet:leading-[60px]">
-          {isLoading ? '로딩 중입니다..' : data?.activities[0].title}
-          {isError && <div>데이터를 불러오는데 실패하였습니다. 다시 시도해주세요.</div>}
-        </h4>
-        <span className="text-md mobile:text-xl tablet:text-2xl">
-          {isLoading ? `${calendarNum}월의 인기 경험 로딩 중입니다...` : `${calendarNum}월의 인기 경험 BEST 🔥`}
-          {isError && <div>데이터를 불러오는데 실패하였습니다.</div>}
-        </span>
-      </div>
+      <Carousel />
     </div>
   );
 }
---
+++ app/_components/Carousel/index.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import React, { useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import Image from 'next/image';
+import Btn from 'public/assets/icons/carousel-btn.svg';
+import Fade from 'embla-carousel-fade';
+import Autoplay from 'embla-carousel-autoplay';
+import useCarouselDotBtn, { DotButton } from './CarouselDotBtn';
+import { EmblaCarouselType } from 'embla-carousel';
+import useGetActivities from '@/_hooks/activities/useGetActivities';
+import Spinner from 'public/assets/icons/spinner.svg';
+
+const calendarNum = new Date().getMonth() + 1;
+
+/**
+ * Carousel 컴포넌트 입니다.
+ */
+
+export default function Carousel() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, duration: 30 }, [Fade(), Autoplay({ delay: 3000 })]);
+
+  const { data, isLoading, isError } = useGetActivities({
+    method: 'cursor',
+    cursorId: null,
+    size: 3,
+    sort: 'most_reviewed',
+  });
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) {
+      emblaApi.scrollPrev();
+    }
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) {
+      emblaApi.scrollNext();
+    }
+  }, [emblaApi]);
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (!autoplay) return;
+
+    const resetOrStop = autoplay.options.stopOnInteraction === false ? autoplay.reset : autoplay.stop;
+
+    resetOrStop();
+  }, []);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useCarouselDotBtn(emblaApi, onNavButtonClick);
+
+  if (isLoading) {
+    return <Image src={Spinner} fill alt="로딩중" />;
+  }
+
+  if (isError) {
+    return <div className="flex h-full items-center justify-center text-3xl font-bold">문제가 발생했습니다!</div>;
+  }
+
+  return (
+    <>
+      <div className="group h-full overflow-hidden" ref={emblaRef}>
+        <div className="flex h-full">
+          {data?.activities.map((activity, i) => (
+            <div key={i} className="relative h-full w-full flex-shrink-0" data-id={activity.id}>
+              <Image src={activity.bannerImageUrl} alt={activity.title} fill className="flex items-center justify-center object-cover" />
+              <div className="relative z-20 flex h-full w-full max-w-[250px] flex-col items-center justify-center break-keep pl-7 text-xl font-bold text-white xl:left-[100px] 2xl:left-[200px] mobile:max-w-[450px] mobile:pl-16 mobile:text-[40px] mobile:leading-[60px] tablet:max-w-[600px] tablet:pl-20 tablet:text-5xl tablet:leading-[60px]">
+                <span className="mb-2 w-full text-left leading-7 mobile:mb-5 mobile:leading-[50px] tablet:leading-[60px]">{activity.title}</span>
+                <span className="w-full text-left text-sm mobile:text-xl tablet:text-2xl">{`${calendarNum}월의 인기 경험 BEST 🔥`}</span>
+              </div>
+              <div className="absolute top-0 z-10 size-full bg-bannerGradient" />
+            </div>
+          ))}
+        </div>
+        <div className="absolute inset-0 flex justify-between mobile:px-4 tablet:px-8">
+          <button onClick={scrollPrev} className="hidden group-hover:block">
+            <Image src={Btn} alt="이전 버튼" width={40} height={40} className="size-8 mobile:size-10" />
+          </button>
+          <button onClick={scrollNext} className="hidden group-hover:block">
+            <Image src={Btn} alt="다음 버튼" width={40} height={40} className="size-8 rotate-180 mobile:size-10" />
+          </button>
+        </div>
+      </div>
+      <div className="dots absolute inset-x-0 top-0 z-50 flex flex-wrap items-center justify-center">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            onClick={() => onDotButtonClick(index)}
+            className={'dot inline-flex h-10 w-7 cursor-pointer touch-manipulation appearance-none items-center justify-center rounded-full bg-transparent'.concat(
+              index === selectedIndex ? ' dot--selected' : '',
+            )}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
