2ac06b10a937627499f45afab99dfd5724707955
i-nooo-2 <168407977+i-nooo-2@users.noreply.github.com>
2024-08-29 00:21:37 +0900

    Fix: Profile Img 없을 경우 검사 후 대체 이미지 적용

--- app/_components/SideUserProfileCard/index.tsx
+++ app/_components/SideUserProfileCard/index.tsx
@@ -67,6 +67,9 @@ export default function SideUserProfileCard() {
 
   useEffect(() => {
     if (UserProfileData) {
+      if (!UserProfileData.profileImageUrl) {
+        UserProfileData.profileImageUrl = DEFAULT_AVATAR;
+      }
       setCurrentAvatarSrc(UserProfileData.profileImageUrl);
       setUserProfile(() => UserProfileData);
     }
@@ -115,7 +118,7 @@ export default function SideUserProfileCard() {
       />
       <div className="flex h-[432px] w-full min-w-[215px] flex-col justify-between gap-6 rounded-xl border border-gray-200 bg-white p-6 shadow-[0_4px_16px_0_rgba(17,34,17,0.05)]">
         <div className="flex justify-center">
-          <AvatarEditBtnWrapper avatarSrc={currentAvatarSrc || DEFAULT_IMAGE} onClick={openModal} />
+          <AvatarEditBtnWrapper avatarSrc={currentAvatarSrc} onClick={openModal} />
         </div>
         <div className="flex flex-col gap-2">
           {profileActionButtons.map((item, idx) => (
