d4edab388438c73d7b837bcd185ccfdf2243e217
i-nooo-2 <168407977+i-nooo-2@users.noreply.github.com>
2024-08-28 15:37:18 +0900

    Chore: Shadcn ui component 환경 구성

--- styles/tailwind.animation.ts
+++ styles/tailwind.animation.ts
@@ -3,6 +3,8 @@ const animation = {
   'fade-out': 'fade-out 0.3s forwards',
   'infinite-slide-original': 'infinite-slide-original 40s linear infinite',
   'infinite-slide-copy': 'infinite-slide-copy 40s linear infinite',
+  'accordion-down': 'accordion-down 0.2s ease-out',
+  'accordion-up': 'accordion-up 0.2s ease-out',
 };
 
 export default animation;
