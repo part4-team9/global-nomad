dfe2abbb8c78822b871154992e1d3c82a28cc1ee
eundonghyeok <mephisto7191@naver.com>
2024-08-28 21:01:44 +0900

    Fix: 페이지네이션 문제 해결

--- app/(route)/main/_components/Pagination/index.tsx
+++ app/(route)/main/_components/Pagination/index.tsx
@@ -2,8 +2,6 @@
 
 import Image from 'next/image';
 
-import usePagination from '@/_hooks/activities/usePagination';
-
 import Button from '../Button';
 
 import ArrowRight from 'public/assets/icons/arrow-right.svg';
@@ -11,27 +9,24 @@ import ArrowRightDisabled from 'public/assets/icons/arrow-right-disabled.svg';
 
 interface PaginationProps {
   currentPage: number;
-  itemsInPage: number;
-  onPageChange: (value: number) => void;
-  totalCount: number;
-  visiblePages?: number;
+  goToNextSet: () => void;
+  goToPage: (page: number) => void;
+  goToPreviousSet: () => void;
+  pageNumbers: number[];
+  totalPages: number;
 }
 
 /**
  * Pagination 컴포넌트 입니다.
  * @param {number} totalCount - 총 데이터의 갯수 입니다.
  * @param {number} itemsInPage - 페이지당 보여지는 아이템의 갯수 입니다.
- * @param {number} visiblePages - 보여질 페이지네이션 버튼의 갯수, 기본값은 5 입니다.
  * @param {function} onPageChange - 현재 페이지 값을 상위 컴포넌트로 보내는 함수 입니다.
  * @param {number} currentPage - 현재 페이지 값 입니다.
  */
 
-export default function Pagination({ totalCount, itemsInPage, visiblePages = 5, onPageChange, currentPage }: PaginationProps) {
-  const totalPages = Math.ceil(totalCount / itemsInPage);
-  const { pageNumbers, goToPage, goToNextSet, goToPreviousSet } = usePagination(totalPages, visiblePages, onPageChange);
-
+export default function Pagination({ totalPages, currentPage, pageNumbers, goToNextSet, goToPage, goToPreviousSet }: PaginationProps) {
   const canGoToPreviousSet = currentPage > 1;
-  const canGoToNextSet = Math.ceil(currentPage / visiblePages) !== Math.ceil(totalPages / visiblePages);
+  const canGoToNextSet = Math.ceil(currentPage / 5) !== Math.ceil(totalPages / 5);
 
   return (
     <div className="mx-[24px] mb-[60px] flex w-full max-w-[1200px] items-center justify-center gap-[10px] mobile:mb-[100px]">
