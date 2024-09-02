cf22d58671f74ece28442d1a17d782d3053f9b39
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2024-09-02 19:10:07 +0900

    Revert "v1.0."
    This reverts commit b3abe41abdf816e2142d91476ae422db33abcd89.

--- app/(route)/my/reservation-status/_component/NoReservation/index.tsx
+++
@@ -1,4 +0,0 @@
-export default function NoReservation() {
-  return <div>예약 내역 없음</div>;
-  // 예약 내역 pr merge시 컴포넌트 삽입
-}
--- app/_apis/reservation/getMyActivities.ts
+++
@@ -1,8 +0,0 @@
-import type { ActivitiesResponse } from '@/_types';
-
-import axiosInstance from '@/_libs/axios';
-
-export default async function getMyActivities(): Promise<ActivitiesResponse> {
-  const response = await axiosInstance.get<ActivitiesResponse>('/my-Activities');
-  return response.data;
-}
