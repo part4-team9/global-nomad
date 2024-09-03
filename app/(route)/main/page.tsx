34a0b6923343f263393e5c0a124738f79d1f6bda
eundonghyeok <mephisto7191@naver.com>
2024-09-03 18:21:58 +0900

    Refactor: framer motion css 수정

--- app/(route)/main/page.tsx
+++ app/(route)/main/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 import AllActivityLists from './_components/AllActivityLists';
 import Banner from './_components/Banner';
@@ -14,13 +15,15 @@ export default function Home() {
   };
 
   return (
-    <div className="box-border flex w-full min-w-full flex-col content-center items-center">
-      <Banner />
-      <SearchBox onSearch={handleSearch} />
-      <div className="relative bottom-[20px] mx-auto w-full px-[16px] lg:max-w-[1200px] mobile:bottom-[20px] mobile:px-[24px]">
-        {searchValue === '' && <HotActivityLists />}
-        <AllActivityLists searchValue={searchValue} />
+    <motion.div initial={{ y: -20, opacity: 0.8 }} animate={{ y: 0, opacity: 1 }}>
+      <div className="box-border flex w-full min-w-full flex-col content-center items-center">
+        <Banner />
+        <SearchBox onSearch={handleSearch} />
+        <div className="relative bottom-[20px] mx-auto w-full px-[16px] lg:max-w-[1200px] mobile:bottom-[20px] mobile:px-[24px]">
+          {searchValue === '' && <HotActivityLists />}
+          <AllActivityLists searchValue={searchValue} />
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
